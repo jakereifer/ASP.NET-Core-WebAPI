be8ab2967a9d25e31ef8c71244686d094bf612be
Jake Reifer <reifer.jake@gmail.com>
2021-07-13 12:18:24 -0400

    Add logging middleware

--- TodoApi/Middleware/LoggingMiddleware.cs
+++ TodoApi/Middleware/LoggingMiddleware.cs
@@ -20,32 +20,38 @@ namespace TodoApi.Middleware {
         }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
-            // Copy original request body to a new stream that allows seeking
-            var requestBodyStream = new MemoryStream();
-            var originalRequestBody = context.Request.Body;
-            await originalRequestBody.CopyToAsync(requestBodyStream);
-            requestBodyStream.Seek(0, SeekOrigin.Begin);
-            // Read and log body
-            var requestBodyText = await new StreamReader(requestBodyStream).ReadToEndAsync();
-            _logger.LogInformation("Request: \n" + requestBodyText);
-            // Seek beginning of stream and set it back as the body
-            requestBodyStream.Seek(0, SeekOrigin.Begin);
-            context.Request.Body = requestBodyStream;
 
-            // Set response body to a memory stream
-            var bodyStream = context.Response.Body;
-            var responseBodyStream = new MemoryStream();
-            context.Response.Body = responseBodyStream;
+            // Keep references to original streams
+            var originalRequestStream = context.Request.Body;
+            var originalResponseStream = context.Response.Body;
 
-            // Call next Middleware
-            await next(context);
+            // Copy the request stream
+            var requestStreamCopy = new MemoryStream();
+            await originalRequestStream.CopyToAsync(requestStreamCopy);
+            requestStreamCopy.Position = 0; // Make sure its at the beginning of the stream
 
-            // Read response body and then reset the stream to the beginning
-            responseBodyStream.Seek(0, SeekOrigin.Begin);
-            var responseBodyText = await new StreamReader(responseBodyStream).ReadToEndAsync();
-            _logger.LogInformation("Response: \n" + responseBodyText);
-            responseBodyStream.Seek(0, SeekOrigin.Begin);
-            await responseBodyStream.CopyToAsync(bodyStream); // why?https://www.sulhome.com/blog/10/log-asp-net-core-request-and-response-using-middleware#:~:text=Log%20Request%20Middleware%20The%20log%20request%20middleware%20extracts,exist.%20Here%20is%20the%20definition%20for%20this%20middleware.
+            var requestReader = new StreamReader(requestStreamCopy);
+            var requestContent = await requestReader.ReadToEndAsync(); // Read it in
+            _logger.LogInformation($"Request Body:\n{requestContent}");
+            requestStreamCopy.Position = 0; // make sure its back at the beginning
+
+            // Set request body pointer to original stream
+            context.Request.Body = requestStreamCopy;
+
+            using (var newResponseStream = new MemoryStream()) {
+                context.Response.Body = newResponseStream;
+                await next(context);
+                // Make sure we read from beginning
+                newResponseStream.Position = 0;
+                var responseReader = new StreamReader(newResponseStream);
+                var responseContent = await responseReader.ReadToEndAsync();
+                _logger.LogInformation($"Response Body:\n{responseContent}");
+
+                newResponseStream.Position = 0;
+                await newResponseStream.CopyToAsync(originalResponseStream);
+                // Set response body pointer to original stream (this one is out of scope and the old one has the proper properties)
+                context.Response.Body = originalResponseStream;
+            }
         }
     }
 
@@ -56,4 +62,7 @@ namespace TodoApi.Middleware {
             return builder.UseMiddleware<LoggingMiddleware>();
         }
     }
+
+
+    // helper methods
 }
